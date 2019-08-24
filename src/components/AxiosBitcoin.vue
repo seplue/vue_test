d497424042f14667b92ed3a904cead02e149d255
seplue <severin.pluess@uzh.ch>
2019-08-24 10:12:27 +0200

    no message

--- src/components/AxiosBitcoin.vue
+++ src/components/AxiosBitcoin.vue
@@ -2,15 +2,26 @@
     <div>
         {{ info }}
         <h1>Bitcoin Price Index</h1>
-        <div
-            v-for="currency in info"
-            class="currency"
+
+        <section v-if="errored">
+            <p>Sorry</p>
+        </section>
+
+        <section v-else>
+            <div v-if="loading">Loading...</div>
+
+            <div
+                    v-else
+                    v-for="currency in info"
+                    class="currency"
             >
-            {{currency.description}}:
-            <span class="lighten">
+                {{currency.description}}:
+                <span class="lighten">
                 <span v-html="currency.symbol"></span>{{ currency.rate_float | currencydecimal }}
             </span>
-        </div>
+            </div>
+
+        </section>
     </div>
 
 </template>
@@ -21,19 +32,27 @@
         name: "AxiosBitcoin",
         data() {
             return {
-                info: null
+                info: null,
+                loading: true,
+                errored: false,
             }
-        },
-        mounted() {
-            axios
-                .get("https://api.coindesk.com/v1/bpi/currentprice.json")
-                .then(response => (this.info = response.data.bpi))
-
         },
         filters: {
             currencydecimal (value) {
                 return value.toFixed(2)
             }
+        },
+        mounted() {
+            axios
+                .get("https://api.coindesk.com/v1/bpi/currentprice.json")
+                .then(response => {
+                    this.info = response.data.bpi
+                })
+                .catch(error => {
+                    console.log(error);
+                    this.errored = true
+                })
+                .finally(() => this.loading = false)
         },
     }
 </script>
